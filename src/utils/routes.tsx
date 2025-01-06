7a3893703912563178df37b023803d74ff917dd0
Mohd Sameer <mesh789736@gmail.com>
2025-01-06 15:42:34 +0530

    modify the router and redirect file in public

--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -3,6 +3,13 @@ import Home from "../home/Home";
 import React from "react";
 import App from "../App";
 import Essentials from "../components/courses/Essentials";
+import Tools from "../components/Tools";
+import Signup from "../components/Signup";
+import Assignments from "../components/Assignment";
+import Notes from "../content/Notes";
+import Mores from "../content/Mores";
+import SupportUs from "../content/SupportUs";
+import JoinUs from "../content/JoinUs";
 
 export const router = createBrowserRouter([
     {
@@ -13,5 +20,32 @@ export const router = createBrowserRouter([
         path: "/Essentials",
         element: <Essentials></Essentials>
     },
-    
+    {
+        path: "/Tools",
+        element: <Tools></Tools>
+    },
+    {
+        path: "/signup",
+        element: <Signup></Signup>
+    }, 
+    {
+        path: "/Assignments",
+        element: <Assignments></Assignments>
+    },
+    {
+        path: "/Notes",
+        element: <Notes></Notes>
+    },
+    {
+        path: "/mores",
+        element: <Mores></Mores>
+    },
+    {
+        path: "/SupportUs",
+        element: <SupportUs></SupportUs>
+    }, 
+    {
+        path: "/JoinUs",
+        element: <JoinUs></JoinUs>
+    }
 ]);
